8fb7ef22b9e17282ec749fda74191639b15019d3
Sviatoslav Zimine <slzdev@gmail.com>
2024-10-13 23:38:16 +0200

    plur js oo concepts ok

--- js/plu_js_oo_concepts/src/6_js_use_js_classes.js
+++ js/plu_js_oo_concepts/src/6_js_use_js_classes.js
@@ -1,2 +1,42 @@
 ///
 printSectionTitle('Using JavaScript Classes');
+
+class Person {
+    static isHumanoid = true;  //static property
+    firstName;
+    lastName;
+    age;
+    #adultAge = 18; // private property
+
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    this.age = age;
+    }
+
+    // Getters and setters for fullName
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+  
+    set fullName(fullName) {
+        let nameParts = fullName.split(' ');
+        this.firstName = nameParts[0];
+        this.lastName = nameParts[1];    
+    }
+
+    #isAdult(){  // private method
+        return this.age >= this.#adultAge;
+    }
+
+    isQualified() {
+        return this.#isAdult();
+    }
+}
+
+let jim = new Person('Jim', 'Cooper', 18);
+
+display(jim);
+display(Person.isHumanoid);
+display(jim.fullName);
+display(jim.isQualified());
---
+++ js/plu_js_oo_concepts/src/7_js_inherit_w_js_classes.js
@@ -0,0 +1,45 @@
+printSectionTitle('Inheritance with JavaScript Classes');
+
+
+class Person {
+    constructor(firstName, lastName, age) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+    }
+    
+    isAdult() {
+      return this.age >= 18;
+    }
+  }
+  
+  class Student extends Person {  //extends
+    courses = [];
+    constructor(firstNmae, lastName, age, courses){
+        super(firstNmae, lastName, age) // calling super
+        this.courses = courses;
+    }
+    
+    enroll(course) {
+      this.courses.push(course);
+    }
+    isAdult() {  //overrode method in child class
+        return this.age >= 21;
+      }
+  }
+  
+  let jim = new Student('Jim', 'Cooper', 18, [{courseId: 'CS101'}, {courseId: 'Math101'}]);
+  let jimPerson = new Person('Jim', 'Cooper', 18);
+
+  display(jim.courses[0]);
+  display(jim.firstName);
+  //display(jim.isAdult());
+
+  display(jim instanceof Student)
+  display(jim instanceof Person)
+  
+
+display(jim.isAdult());
+display(jimPerson.isAdult());
+
+
