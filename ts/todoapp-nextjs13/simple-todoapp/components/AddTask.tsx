a16d36fed52efb8e46794a1a34dcc51ac320d275
sviatoslav zimine <sviatoslav.zimine@alpiq.com>
2024-10-22 23:50:20 +0200

    nextjs_todo-simpleapp start

---
+++ ts/todoapp-nextjs13/simple-todoapp/components/AddTask.tsx
@@ -0,0 +1,11 @@
+import {AiOutlinePlus} from 'react-icons/ai'
+const AddTask = () => {
+return (
+    <div>
+       <button className="btn btn-primary w-full">
+        Add new Task <AiOutlinePlus size={10}/>
+        </button>
+    </div>
+)};
+
+export default AddTask;
---
+++ ts/todoapp-nextjs13/simple-todoapp/pages/index.tsx
@@ -0,0 +1,14 @@
+import AddTask from "../components/AddTask";
+
+export default function Home() {
+    return (
+      <>
+        <main className="max-w-4xl mx-auto  mt-4">
+        <div className="text-center my-5 flex flex-col gap-4">
+          <h1 className="text-2xl font-bold">ToDo List App</h1>
+        <AddTask />
+        </div>
+        </main>
+      </>
+    )
+}
